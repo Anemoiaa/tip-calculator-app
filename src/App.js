eb7e3f5f98456615fb48b8c4fdacfe0cdd00f310
anemoiaa <georgiypetroff@gmail.com>
2022-12-02 19:38:31 +0400

    base structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import iconDollar from "./assets/images/icon-dollar.svg";
+import iconPerson from "./assets/images/icon-person.svg";
+import ResetButton from "./components/UI/button/ResetButton/ResetButton";
+import Results from "./components/Results/Results";
+import Header from "./components/Header/Header";
+import Bill from "./components/Bill/Bill";
+import NumOfPeople from "./components/NumOfPeople/NumOfPeople";
+import SelectTip from "./components/SelectTip/SelectTip";
+
 function App() {
     return (
-        <div className="App">
-            <h1>Hello</h1>
+        <div className="wrapper">
+            <Header />
+            <div className="container">
+                <div className="left">
+                    <Bill  icon={iconDollar} />
+                    <SelectTip />
+                    <NumOfPeople icon={iconPerson} />
+                </div>
+                <div className="results-wrapper">
+                    <div className="results">
+                        <Results title={"Tip Amount"} />
+                        <Results title={"Total"} />
+                        <ResetButton />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
